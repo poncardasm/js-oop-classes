b71000776db3c7a0cd6bbee680baf09ebb42edc3
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-12-06 21:42:22 +0200

    Add 01-classes script.js

---
+++ 01-classes/script.js
@@ -0,0 +1,31 @@
+class Rectangle {
+  constructor(name, width, height) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.height * this.width;
+  }
+
+  perimeter() {
+    return 2 * (this.height + this.width);
+  }
+
+  log() {
+    console.log(
+      `Name: ${this.name}, width: ${this.width}, height: ${this.height}`
+    );
+  }
+}
+
+const rect1 = new Rectangle('Rect 1', 25, 20);
+const rect2 = new Rectangle('Rect 2', 10, 5);
+
+console.log(rect1);
+console.log(rect1.name);
+console.log(rect1.area());
+console.log(rect1.perimeter());
+rect1.log();
+rect2.log();
